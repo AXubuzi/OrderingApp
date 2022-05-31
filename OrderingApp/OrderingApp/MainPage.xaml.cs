c2805d84246a704330fb99edc999459ff54afca2
AXubuzi <102726899+AXubuzi@users.noreply.github.com>
2022-05-31 15:47:04 +0200

    Add project files.

---
+++ OrderingApp/OrderingApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OrderingApp
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnSaveClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnCancelClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
